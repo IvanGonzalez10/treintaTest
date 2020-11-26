6f5da581be946fffd6431be6031129633f92f2cb
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-11-26 17:36:59 -0500

    cerrar sesion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { Header } from './components/Header/Header';
 import  Home  from  './pages/Home/Home';
-import {LoginWithGoogle} from './components/Auth/Login/Login';
+import Login from './components/Auth/Login/Login';
 import {Person} from './components/Auth/Login/Login';
 
 
@@ -11,7 +11,7 @@ export const App = () => (
     <GlobalStyle />
     <Header />
     <Home />
-    <LoginWithGoogle />
+    <Login />
     <Person />
   </>
 );
--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,72 +1,74 @@
-import React, { useState } from 'react';
-import withFirebaseAuth from 'react-with-firebase-auth';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from '../configuration/ConfigFirebase';
-
+import React, { useState } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "../configuration/ConfigFirebase";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export const LoginWithGoogle = (props) => {
-    const { user, signOut, signInWithGoogle } = props;
-    return(
-      <div>
-        {
-          user ? <p>Bienvenido, {user.displayName}</p> : <p>Por favor loggeate</p>
-        }
-        {
-          user ? <button onClick={signOut}>Sign Out</button> : <button onClick={signInWithGoogle}>Sign In with Google</button>
-        }
-      </div>
-      
-    )
-  }
+const Login = (props) => {
+  const { user, signOut, signInWithGoogle } = props;
+  return (
+    <div>
+      {user ? <p>Bienvenido, {user.displayName}</p> : <p>Por favor loggeate</p>}
+      {user ? (
+        <button onClick={signOut}>Sign Out</button>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign In with Google</button>
+      )}
+    </div>
+  );
+};
 
 export const Person = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
 
   const submit = async () => {
     await firebase.auth().createUserWithEmailAndPassword(email, password);
-  }
+  };
 
-   firebase.auth().onAuthStateChanged((user) => user && (setUser(user.email)))
+  firebase.auth().onAuthStateChanged((user) => user && setUser(user.email));
 
-   const logOut = async () => {
-     await firebase.auth().signOut();
-   }
+  const Log = async () => {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+  };
 
-   const LogIn = async () => {
-     await firebase.auth().signInWithEmailAndPassword(email, password)
-   }
+  const logout = async () => {
+    await firebase.auth().signOut();
+  };
 
-  return(
+  return (
     <div>
-        {
-        !user &&
-      <div>
+      {!user && (
+        <div>
           <label htmlFor="email">Correo</label>
-          <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label htmlFor="password">password</label>
-          <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button onClick={submit}>Registro</button>
-          <button onClick={LogIn}>Iniciar Sesión</button>
-      </div>
-      }
-      {
-        user && <button onClick={logOut}>Cerrar Sesión</button>
-      }
+          <button onClick={Log}>Iniciar Sesión</button>
+        </div>
+      )}
+      {user && <button onClick={logout}>Cerrar Sesión</button>}
     </div>
-  )
-}
+  );
+};
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
-  googleProvider : new firebase.auth.GoogleAuthProvider(),
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
-
-export default withFirebaseAuth ({
+export default withFirebaseAuth({
   providers,
   firebaseAppAuth,
-})
+})(Login);
